c9c4126235f37974be847fd3978dab34c507a8d0
Deepanjan Kowar <deepanjan.kowar@gmail.com>
2025-06-16 17:46:43 +0530

    Fix imports and update README

--- src/main/kotlin/com/dispatch/app/Application.kt
+++ src/main/kotlin/com/dispatch/app/Application.kt
@@ -2,14 +2,13 @@ package com.dispatch.app
 
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.routing.routing
-import io.ktor.server.application.Application
-import io.ktor.server.application.call
-import io.ktor.server.response.respond
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
 import io.ktor.server.request.*
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.http.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
